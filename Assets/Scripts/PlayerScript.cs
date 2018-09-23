827a40e8bc24c23b87a28501d7211c2247798c7c
Ivan Borisenko <mteru00@gmail.com>
2018-09-23 21:10:41 +0300

    Woman basic movement added

---
+++ Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum GameState { PREVIEW, GAME, PAUSE };
+
+public class PlayerScript : MonoBehaviour {
+
+    public float cameraSpeed = 0.1f;
+    public GameState state = GameState.GAME;
+
+    Vector3 start = new Vector3(0, 1, -6);
+    Vector3 destination = new Vector3(0, 2, -1);
+
+    void Start () {
+		if (this.state == GameState.GAME)
+        {
+            this.MoveCameraToGamePosition();
+        }
+	}
+	
+	void Update () {
+        
+    }
+
+    void MoveCameraToGamePosition()
+    {
+        transform.Translate(this.destination);
+        transform.Rotate(45, 0, 0);
+    }
+}
---
+++ Assets/Scripts/WomanScript.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum WomanState { Start, Moving, End };
+
+public class WomanScript : MonoBehaviour {
+
+    public WomanState state = WomanState.Start;
+
+    public Transform target;
+    public float speed;
+
+    Animator animator;
+    
+    void Start () {
+        animator = GetComponent<Animator>();
+    }
+	
+	void Update () {
+        switch (this.state)
+        {
+            case WomanState.Start:
+                this.MoveToFinalPoint();
+                this.state = WomanState.Moving;
+                break;
+            case WomanState.Moving:
+                this.MoveToFinalPoint();
+                this.CheckForMovingEnd();
+                break;
+            case WomanState.End:
+                break;
+        }
+    }
+
+    void MoveToFinalPoint ()
+    {
+        float step = speed * Time.deltaTime;
+        transform.position = Vector3.MoveTowards(transform.position, target.position, step);
+    }
+
+    void CheckForMovingEnd()
+    {
+        if (this.gameObject.transform.position == this.target.position)
+        {
+            this.state = WomanState.End;
+            animator.SetTrigger("Idle");
+        }
+    }
+}
